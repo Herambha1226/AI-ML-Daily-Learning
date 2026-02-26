e9179a39d95a6b4708b1eb841b944939bb2a9a5a
Herambha1226 <pallapothugayathri23@gmail.com>
2026-02-26 13:45:25 +0530

    simple problem

---
+++ seaborn/practice2.py
@@ -0,0 +1,27 @@
+""" 
+ay 2: Basic Plots
+
+Core plots:
+
+sns.scatterplot()
+
+sns.lineplot()
+
+Practice mini project:
+
+Tips dataset analysis
+
+Bill vs Tip visualization"""
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+df = sns.load_dataset("tips")
+
+fig, ax = plt.subplots(1,2)
+
+sns.scatterplot(ax=ax[0],x="total_bill",y="tip",data=df)
+ax[0].set_title("Total Bill vs Tip (Scatter Plot)")
+
+sns.histplot(ax=ax[1],x="tip",data=df)
+ax[1].set_title("Tip Distribution (Histogram)")
+plt.show()
