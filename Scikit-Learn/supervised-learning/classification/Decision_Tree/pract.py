4f16cef796c97b42e6f536274e16186e0439c25f
Herambha1226 <pallapothugayathri23@gmail.com>
2026-03-26 11:57:53 +0530

    Learning Decision Tree From Scratch

---
+++ Scikit-Learn/supervised-learning/classification/Decision_Tree/pract.py
@@ -0,0 +1,70 @@
+import numpy as np 
+from collections import Counter
+
+x = np.array([
+    ["sunny"],
+    ["sunny"],
+    ["Rainy"],
+    ["Rainy"]
+])
+y = np.array(["No","No","Yes","Yes"])
+
+
+def entropy(y):
+    counts = Counter(y)
+    print("Counts : ",counts)
+    total =  len(y)
+    print("Total Size : ",total)
+
+    ent = 0
+    for count in counts.values():
+        p = count / total
+        ent += -p * np.log2(p)
+        print("Entropy : ",ent)
+    return ent
+
+def information_gain(x_column,y):
+    parent_entropy = entropy(y)
+
+    values = np.unique(x_column)
+    weighted_entropy = 0
+
+    for val in values:
+        indices = np.where(x_column == val)
+        subset_y = y[indices]
+
+        weighted = len(subset_y) / len(y)
+        weighted_entropy += weighted * entropy(subset_y)
+
+        return parent_entropy - weighted_entropy
+    
+def best_split(x,y):
+    best_gain = -1 
+    best_feature = None
+
+    for col in range(x.shape[1]):
+        gain = information_gain(x[:,col],y)
+        print(f"Feature {col} Gain : {gain}")
+
+        if gain > best_gain:
+            best_gain = gain
+            best_feature = col
+    return best_feature
+
+def build_tree(x,y):
+    if len(set(y)) == 1:
+        return y[0]
+    
+    feature = best_split(x,y)
+
+    tree = {}
+    values = np.unique(x[:,feature])
+    for val in values:
+        indices = np.where(x[:,feature] == val)
+        subtree = build_tree(x[indices],y[indices])
+
+        tree[val] = subtree
+    return tree
+
+tree = build_tree(x,y)
+print("Decison Tree : ",tree)
